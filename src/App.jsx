24726942db33c5017f05b9676e5d5c48bf4071c5
xMAPKEPx <russia201554321@gmail.com>
2023-11-19 17:17:51 +0500

    created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/table/Table';
+import ChartComponent from './components/chart/chart';
+
+function App() {
+  return <>
+  <div className="App">
+      
+      <aside>
+        <Table />
+      </aside>
+      <main>
+        <ChartComponent />
+      </main>
+    </div>
+  </>;
+}
+
+export default App;
