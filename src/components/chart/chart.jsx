b476ba27749fe4eaa5d87e8390572b5f7f90f656
xMAPKEPx <russia201554321@gmail.com>
2023-12-13 14:20:43 +0500

    Корректировки в графике

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,42 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import testResponse from '../../assets/response_1701160657806.json'
+import testResponse from '../../assets/response_2.json'
 
+// Компонент-график
 const ChartComponent = () => {
+  // Состояния для данных и статуса загрузки
   const [data, setData] = useState([]);
-
+ 
+  // Загрузка данных из API
   useEffect(() => {
-    fetch('https://api.example.com/data')
-      .then(response => response.json())
-      .then(data => setData(data));
-  }, []);
-
-  const COLORS = ['#00008B', '#ffffff', '#000000', '#123456', '#654321', '#ccc000']
-  function getRandomInt (min,max) {
-      let rand = min + Math.random() * (max + 1 - min);
-      return Math.floor(rand);
+     fetch('https://api.example.com/data')
+       .then(response => response.json())
+       .then(data => setData(data));
+  }, [data]);
+ 
+  // Массив цветов
+  const COLORS = ['#00008B', '#ffffff', '#123456', '#654321', '#ccc000']
+ 
+  // Функция генерации случайного цвета
+  function getRandomInt(min, max) {
+     let rand = min + Math.random() * (max + 1 - min);
+     return Math.floor(rand);
   }
-
+ 
+  // Отрисовка
   return <>
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        data={testResponse}
-        margin={{ top: 50, right: 30, left: 20, bottom: 55 }}
-      >
-        <XAxis dataKey="id" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-
-        {testResponse.map((s) => (
-          <Line isAnimationActive={false} dot={false} dataKey="value" data={s.data} name={s.name} key={s.name} stroke={COLORS[getRandomInt(0, COLORS.length-1)]} strokeWidth={3} />
-        ))}
-
-        {data}
-      </LineChart>
-    </ResponsiveContainer>
-    </>;
-}
-
-export default ChartComponent;
+       <ResponsiveContainer width="100%" height="100%">
+         <LineChart
+           data={testResponse}
+           margin={{ top: 50, right: 30, left: 20, bottom: 55 }}
+         >
+           {/* Ось X */}
+           <XAxis dataKey="id" type='category' stroke='white' allowDuplicatedCategory={false} />
+ 
+           {/* Ось Y */}
+           <YAxis dataKey="value" stroke='white' />
+ 
+           {/* Сетка */}
+           <CartesianGrid strokeDasharray="0 0" />
+ 
+           {/* Подсказка при наведении */}
+           <Tooltip />
+ 
+           {/* Легенда графика */}
+           <Legend />
+ 
+           {/* Элементы линий */}
+           {testResponse.map((s) => (
+             <Line
+               type="monotone"
+               isAnimationActive={false}
+               dot={false}
+               dataKey="value"
+               data={s.data}
+               name={s.name}
+               key={s.name}
+               stroke={COLORS[getRandomInt(0, COLORS.length - 1)]}
+               strokeWidth={3}
+             />
+           ))}
+ 
+           {/* Отрисовка дополнительных данных */}
+           {data}
+         </LineChart>
+       </ResponsiveContainer>
+     </>
+ }
+ 
+ // Экспорт компонента
+ export default ChartComponent;
