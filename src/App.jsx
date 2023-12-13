ffa7a5241e4938971c425fbdb1c3c3acc6931500
xMAPKEPx <russia201554321@gmail.com>
2023-12-13 14:52:01 +0500

    popup on load

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 import './App.css';
 import Table from './components/table/Table';
 import ChartComponent from './components/chart/chart';
+import PopupLoad from './components/onLoadPopup/popup';
 
 function App() {
   return <>
   <div className="App">
-      
+      <div className='dark-content'>
+      <PopupLoad />
+      </div>
       <aside>
         <Table />
       </aside>
---
+++ src/components/onLoadPopup/popup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PopupLoad = () => {
+    const onClickHandler = () => {
+        alert("Сессия 1 выбрана")
+        document.querySelector('.dark-content').classList.add('hidden')
+    };
+
+    return <div className="popup">
+        <h1>Выберите сессию</h1>
+        <button onClick={onClickHandler}>Сессия 1</button>
+    </div>
+};
+
+export default PopupLoad;
